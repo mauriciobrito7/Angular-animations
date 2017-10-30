fd506910d3b8076b4255e19f81860da269ab3030
mauriciobrito7 <maubot01@gmail.com>
2017-10-30 19:21:50 -0400

    angular animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { Hero } from './shared/hero';
+import { HeroesService } from './services/hero.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [HeroesService, Hero],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
